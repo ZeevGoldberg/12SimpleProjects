3299037ceb3da152bf0218e82b51e65c0467e929
Zeev Y. Goldberg <zeevygoldberg@gmail.com>
2022-09-06 10:24:17 +0300

    after Mosh sub

---
+++ digital clock/index.js
@@ -0,0 +1,8 @@
+const hourEl = document.getElementById("hour");
+ const minuteEl = Document.getElementById("minutes");
+ const secondsEl = Document.getElementById("seconds");
+ const ampmEl = Document.getElementById("ampm");
+
+ function updateClock (){
+    
+ }
